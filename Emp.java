b72d3927715a35ecc69701e97ef6e5b6b0fc6669
Nalawade Savita <75830608+savita2000@users.noreply.github.com>
2021-02-14 18:54:57 +0530

    Create Emp.java

---
+++ Emp.java
@@ -0,0 +1,22 @@
+package mycaptain;
+
+public class Employee 
+   {
+	public String name,address;
+    public int salary;
+    
+	
+    public static void main(String[] args) 
+	{    
+		System.out.println("Name               Year of joining");
+		System.out.println("Address");
+		System.out.println("Robert                 1994               64C-");
+		System.out.println("WallsStreet");
+		System.out.println("Sam                   2000               68D-");
+		System.out.println("WallsStreet");
+		System.out.println("john                  1999              26B-");
+		System.out.println("WallsStreet");
+
+	}
+
+}
